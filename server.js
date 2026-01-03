3772a66b03c12fb27b42edcdc8b6979300ab40f7
Viswa Jith <viswa@gmail.com>
2026-01-03 11:46:19 +0530

    Add health check route

--- server.js
+++ server.js
@@ -27,9 +27,10 @@ mongoose
   });
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
