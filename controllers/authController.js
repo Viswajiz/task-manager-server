93a7dd686efa7664527808e622319c496160a4e7
Viswa Jith <viswa@gmail.com>
2026-01-03 16:26:20 +0530

    Handle duplicate email error in register API

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,12 +3,17 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-  await User.create({ name, email, password: hashedPassword });
+    const user = await User.create({ name, email, password });
 
-  res.json({ message: "User registered" });
+    res.status(201).json({
+      message: "User registered successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 exports.login = async (req, res) => {
