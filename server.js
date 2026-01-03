b3059896433e89da4445bafb73bf3d718a8561ec
Viswa Jith <viswa@gmail.com>
2026-01-03 11:37:33 +0530

    Fix MongoDB connection for Render

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
 
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
@@ -18,13 +17,15 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-// DB connection
+// DB
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
-  })
-  .catch((err) => console.log(err));
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
