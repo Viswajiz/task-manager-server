d8b6fb959b017b2e1373c8fb621245777fa14f8f
Viswa Jith <viswa@gmail.com>
2026-01-03 11:43:52 +0530

    Add dummy backend response for Render

--- server.js
+++ server.js
@@ -29,3 +29,7 @@ mongoose
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
